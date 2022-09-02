d8ac50c6bf0ac7b210b1d172a15c416442081a29
Surya <surya.kumar@zohocorp.com>
2022-09-03 01:32:53 +0530

    Initial Commit

---
+++ BazelCheckUITests/BazelCheckUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BazelCheckUITestsLaunchTests.swift
+//  BazelCheckUITests
+//
+//  Created by surya-zstk231 on 03/09/22.
+//
+
+import XCTest
+
+class BazelCheckUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'BazelCheck' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for BazelCheck
+
+  target 'BazelCheckTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'BazelCheckUITests' do
+    # Pods for testing
+  end
+
+end
